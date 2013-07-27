bae09f93bd233e55af2f9ba59c68bf7ad17cfa1f
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-07-27 19:02:51 -0400

    Refactor inline headers

--- lib/scribe/lookup.ex
+++ lib/scribe/lookup.ex
@@ -1,12 +1,7 @@
 defmodule Scribe.Lookup do
-  @markdown_single_line [{"#", "h1"},
-                         {"##", "h2"},
-                         {"###", "h3"},
-                         {"####", "h4"},
-                         {"#####", "h5"},
-                         {"######", "h6"}]
 
-  def markdown(tag) do
-    @markdown_single_line[tag]
+  def markdown(tag="#"<>_) do
+    "h#{String.length tag}"
   end
+
 end
