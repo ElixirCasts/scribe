39a6b4499383ee337ac164635f5b440d70ea14c6
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-07-27 14:02:29 -0400

    Add package.exs for expm support.

---
+++ package.exs
@@ -0,0 +1,11 @@
+Expm.Package.new(
+  name: "Scribe",
+  description: """Scribe compiles Markdown to HTML.
+                  Scribe will integrate with apprentice""",
+  version: "0.0.1",
+  keywords: ["Elixir", "markdown", "apprentice"],
+  maintainers: [[name: "Joe Jackson",
+                 email: "joe.jackson@scimedsolutions.com"],
+                [name: "Michael Simpson"
+                 email: "mjs2600@gmail.com"]],
+  repositories: [[github: "ElixirCasts/scribe"]])
