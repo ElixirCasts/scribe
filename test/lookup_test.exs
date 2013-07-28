881e7f3a63751836f2a2d7ca83654ad2112e1cf2
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-07-28 10:49:02 -0400

    Fix text in tests.

--- test/lookup_test.exs
+++ test/lookup_test.exs
@@ -3,49 +3,49 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule LookupTest do
   use ExUnit.Case
 
-  test "Knows how to handle #" do
+  test "knows how to handle #" do
     markdown = Scribe.Lookup.markdown("#")
     heading = "h1"
     assert markdown == heading
   end
 
-  test "Knows how to handle ##" do
+  test "knows how to handle ##" do
     markdown = Scribe.Lookup.markdown("##")
     heading = "h2"
     assert markdown == heading
   end
 
-  test "Knows how to handle ###" do
+  test "knows how to handle ###" do
     markdown = Scribe.Lookup.markdown("###")
     heading = "h3"
     assert markdown == heading
   end
 
-  test "Knows how to handle ####" do
+  test "knows how to handle ####" do
     markdown = Scribe.Lookup.markdown("####")
     heading = "h4"
     assert markdown == heading
   end
 
-  test "Knows how to handle #####" do
+  test "knows how to handle #####" do
     markdown = Scribe.Lookup.markdown("#####")
     heading = "h5"
     assert markdown == heading
   end
 
-  test "Knows how to handle ######" do
+  test "knows how to handle ######" do
     markdown = Scribe.Lookup.markdown("######")
     heading = "h6"
     assert markdown == heading
   end
 
-  test "Knows how to handle =" do
+  test "knows how to handle =" do
     markdown = Scribe.Lookup.markdown("======")
     heading = "h1"
     assert markdown == heading
   end
 
-  test "Knows how to handle -" do
+  test "knows how to handle -" do
     markdown = Scribe.Lookup.markdown("------")
     heading = "h2"
     assert markdown == heading
